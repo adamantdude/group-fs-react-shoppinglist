a74729e201bffeac7f0cb1188494d53ac5b6d5d6
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-20 11:51:10 -0500

    first test of GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import {useState,useEffect} from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 import axios from 'axios';
 import AddItem from '../AddItem/AddItem.jsx';
+import GroceryList from '../GroceryList/GroceryList.jsx';
+
 
 function App() {
+
+    let [groceryList,setGroceryList] = useState([]);
+
+    useEffect(()=>{
+        getItems();
+    },[]);
 
     const addItem = (item) => {
         axios({
@@ -21,6 +29,20 @@ function App() {
             })
     }
 
+    const getItems = ()=>{
+        axios({
+            method: 'GET',
+            url: '/LIST'
+        })
+        .then((response)=>{
+            setGroceryList(response.data);
+            console.log('got items', response.data);
+        })
+        .catch((err)=>{
+            console.error('error in axios GET',err);
+        });
+    };
+
     return (
         <div className="App">
             <Header />
@@ -28,6 +50,10 @@ function App() {
                 <p>Under Construction...</p>
             </main>
             <AddItem addFn={addItem}/>
+            <h2>Shopping List</h2>
+            <button>Reset</button>
+            <button>Clear</button>
+            <GroceryList groceryList={groceryList} />
         </div>
     );
 }
